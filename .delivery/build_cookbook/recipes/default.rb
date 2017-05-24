dce6679623a7b7d915b0506c48674f99d50ebc42
Marc Arndt <marc@marcarndt.com>
2017-05-24 11:12:39 +0200

    setting sonatype repo

--- .delivery/build_cookbook/recipes/default.rb
+++ .delivery/build_cookbook/recipes/default.rb
@@ -5,3 +5,13 @@
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
 include_recipe 'coffee-truck::default'
+
+secrets = get_project_secrets
+
+maven_settings "settings.servers" do
+  value "server" => {
+      "id" => "ossrh",
+      "username" => "zamedic",
+      "password" => "#{secrets['osspass']}"
+  }
+end
